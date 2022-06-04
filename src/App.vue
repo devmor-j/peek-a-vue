5f498c82fff19f5550552bfcea4dbc9868f6f0e9
Morteza Jamshidi <devmor.j@gmail.com>
2022-06-04 14:59:19 +0430

    refactor: abstract game title

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { onMounted } from "vue";
 import GameCard from "@/components/GameCard";
 import GameButton from "@/components/GameButton";
+import GameTitle from "@/components/GameTitle";
 import createDeck from "./features/createDeck";
 import createGame from "./features/createGame";
 import { halloweenDeck } from "./data/halloweenDeck.json";
@@ -17,6 +18,7 @@ export default {
   components: {
     GameCard,
     GameButton,
+    GameTitle,
   },
 
   setup() {
@@ -56,8 +58,7 @@ export default {
 </script>
 
 <template>
-  <h1 class="visually-hidden">Peek-a-Vue</h1>
-  <img src="/images/peek-a-vue-title.png" alt="Peek a vue" />
+  <GameTitle />
 
   <TransitionGroup tag="section" name="shuffle-transition" class="game-board">
     <GameCard
@@ -109,16 +110,6 @@ body {
   gap: 1.25rem;
   justify-content: center;
   padding-block: 2rem;
-}
-
-.visually-hidden:not(:focus):not(:active) {
-  clip: rect(0 0 0 0);
-  clip-path: inset(50%);
-  height: 1px;
-  overflow: hidden;
-  position: absolute;
-  white-space: nowrap;
-  width: 1px;
 }
 
 .shuffle-transition {
---
+++ src/components/GameTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <h1 class="visually-hidden">Peek-a-Vue</h1>
+  <img src="/images/peek-a-vue-title.png" alt="Peek a vue" />
+</template>
+
+<style scoped>
+.visually-hidden:not(:focus):not(:active) {
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+}
+</style>
